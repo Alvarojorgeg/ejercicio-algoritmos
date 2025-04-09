50f5f387db3bd03efaaf925cd7d123a325a6956e
Álvaro <145665238+Alvarojorgeg@users.noreply.github.com>
2025-04-09 09:07:17 +0000

    agregado main global

---
+++ main GLOBAL.py
@@ -0,0 +1,9 @@
+import os
+
+print("\nEJECUTANDO TODOS LOS EJERCICIOS...\n")
+
+print("---- EJERCICIO 1 ----")
+os.system("python ejercicio1/main.py")
+
+print("\n---- EJERCICIO 2 ----")
+os.system("python ejercicio2/main.py")
